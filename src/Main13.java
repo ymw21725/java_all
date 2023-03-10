61f37c4701c6b531bef8ebb612ae71b5baf6bf43
YMW\did40 <ymw21725@gmail.com>
2023-03-10 13:11:33 +0900

    재귀호출.

--- src/Main13.java
+++ src/Main13.java
@@ -1,18 +1,20 @@
-public class Main13 {	// 재귀호출.시점 기준으로 호출 이전과 이후 구분할줄 알아야 한다.
-
-	public static void output(int n) {
-		if(n<=0) {
-			System.out.println();
-			return;
-		}
-		
-		System.out.println("재귀호출 직전까지 코드는 호출 순서대로 : " + n);	// 3,2,1
-		output(n-1);
-		System.out.println("재귀호출 직후부터 코드는 호출 역순으로 : " + n); // 1,2,3
-	}
-		
-	public static void main(String[] args) {
-		output(3);
-
-	}
+public class Main13{
+   public static void output(int n) {
+      if( n<=0 ) {               // 재귀호출 탈출 코드.   
+         System.out.println();
+         return;
+      }
+      
+      System.out.println("( 재귀호출 직전까지의 코드는 호출 순서대로 진행 ),  n = " + n );
+      
+      output(n-1);   // 당행의 재귀호출 시점까지의 모든 코드 실행은 최종 재귀호출 탈출 지점까지 호출 순서대로 진행됨을 확인 가능.
+                  // 단, 호출 완료후 원 호출시점으로의 복귀 순서는 최종 재귀호출 탈출 코드를 통해 역순으로 진행됨에 따라 14행
+                  // 처럼 당행의 재귀호출 완료후의 코드 실행은 호출 순서의 역순으로 진행됨을 확인 가능.
+      
+      System.out.println("( 재귀호출 직후부터의 코드는 호출 역순으로 진행 ),  n = " + n);
+   }
+   
+   public static void main(String[] args) {
+      output(3);
+   }
 }
