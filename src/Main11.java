d07cb823d241a33bd7f3b5b1b3860aa913b853f9
YMW\did40 <ymw21725@gmail.com>
2023-03-10 12:44:19 +0900

    메소드 오버로딩

--- src/Main11.java
+++ src/Main11.java
@@ -1,6 +1,6 @@
 public class Main11 {
 
-	static int add(int... a) {	// 가변인수는 타입명... 형식인수 방식으로 사용해야한다.
+	static int add(int...a) {	// 가변인수는 타입명... 형식인수 방식으로 사용해야한다.
 		int s = 0;				// 동일타입의 인수 여러개를 배열형태로 전달받음.
 		
 		for(int i:a) s += i;
