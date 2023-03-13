cad1b8f6bcf1e48a7b20baecf2c996e122c8115c
YMW\did40 <ymw21725@gmail.com>
2023-03-13 09:54:10 +0900

    재귀호출 역순 (첫번째 max값 기억해서 하기.)

--- src/Main18.java
+++ src/Main18.java
@@ -1,16 +1,24 @@
-import java.util.Scanner;	// 호출 역순으로 하기/. 리턴이 어디있느냐에따라..
+import java.util.Scanner;	// 호출 역순으로 하기/. 재귀호출 후 불러오냐 그전에 재귀호출 전에 불러오냐에 따라 역순인지 아닌지 구분.
 
 public class Main18{
-   public static int validateSum(int t, int min,int max) {
-	   
-	  if(min>max) return t;
-
-	  t = validateSum(t, min, max-1);
+   public static int validateSum(int min,int max,int firstMax) {	// 1,10이라고 가정.
+	  int tot;
 	  
-	  System.out.printf("%d%s",max,min<max ? " \n " : " + ");
-	  t+=max;
-
-	  return t;  
+//	  if(min>max) return t;
+//
+//	  t = validateSum(t, min, max-1);
+//	  
+//	  System.out.printf("%d%s",max,min<max ? " + " : " \n ");
+//	  t+=max;
+//
+//	  return t;  
+	   
+//	   return 전누적합 + 현재대상값
+	   if(max<min) return 0;
+	   tot = validateSum(min,max - 1,firstMax) + max;
+	   
+	   System.out.printf("%d%s",max,max<firstMax ? " + " : "\n\n");
+	   return tot;
    }
    
    public static void main(String[] args) {
@@ -39,6 +47,6 @@ public class Main18{
       System.out.printf("<루프 이용 검증>%d부터 %d까지의 합은 : %d\n\n", min, max, tot);
       
       
-      System.out.printf("\n<재귀호출을 이용한 누적합> %d부터 %d까지의 합은 : %d", min, max, validateSum(0,min,max) );
+      System.out.printf("\n<재귀호출을 이용한 누적합> %d부터 %d까지의 합은 : %d", min, max, validateSum(min,max,max) );
    }
 }
