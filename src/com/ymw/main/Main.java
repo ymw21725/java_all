e53ca9561bb8b1ce5b11a4ccabf365a3fb7e868a
양민우 <did405@naver.com>
2023-03-14 10:42:29 +0900

    sub2는 private이므로 같은클래스가 아니므로 불러올 수 없다.

--- src/com/ymw/main/Main.java
+++ src/com/ymw/main/Main.java
@@ -1,10 +1,18 @@
 package com.ymw.main;
 import com.ymw.sub1.Sub1;
+
+class Sub2 {
+	private void test() {
+		System.out.println("sub2");
+	}
+}
+
 // 다른 패키지 불러오려면 import해서 경로 지정해줘야한다.
 public class Main {
 	public static void main(String[] args) {
 		Sub1 sub1= new Sub1();
+		Sub2 sub2= new Sub2();
 		
-		sub1.test();
+//		sub2.test();
 	}
 }
